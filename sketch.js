ff15942f10ff8bc62986b7b86694e055744eb8c1
fierytwo <166926139+fierytwo@users.noreply.github.com>
2024-04-13 16:34:02 +0200

    Update sketch.js to full screen

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ let r;             //initializing variables
 let totalPoints;
 let factor;
 function setup() {
-  createCanvas(500, 500);
+  createCanvas(windowWidth, windowHeight);
   colorMode(HSB)//creating screen
 r = width/2 - 10;          //setting r value
 
